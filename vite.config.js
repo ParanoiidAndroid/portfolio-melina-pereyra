64fc3444da5d9d506a72b9e92c1fbc7ae23a57ca
paranoiidandroid <fernandezmartin156@gmail.com>
2025-10-22 21:06:11 -0300

    agrego base en vite.config para el correcto deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from "node:url";
 
   // https://vite.dev/config/
   export default defineConfig({
+    base: "/portfolio-melina-pereyra/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
